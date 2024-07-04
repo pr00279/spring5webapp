a8af441759c8346606ba46dd563b518b0f549cc8
Phoebe Revolta <phoebe.revolta@solirius.com>
2024-07-04 13:35:58 +0100

    assignment-2: add changes from Thymeleaf Templates lesson

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -39,7 +39,6 @@ public class Publisher {
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
-                ", books=" + books +
                 '}';
     }
 
